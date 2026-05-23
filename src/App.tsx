5a0a50d4fb82bbcf2b981e868bf7c08ba19dcab1
BelattiMateo <mateobelatti7@gmail.com>
2026-05-23 10:07:14 -0300

    aplicacion de rutas a pages y actulizacion de dependencias

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/layotus/header';
 import Footer from './components/layotus/footer';
 import { HomePage } from './pages/homePage';
+import ContactPage from './pages/contactoPage';
+import ProductPage from './pages/productPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <main>
         <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/contacto' element={<ContactPage />} />
+          <Route path='/producto' element={<ProductPage />} />
         </Routes>
       </main>
       <Footer />
